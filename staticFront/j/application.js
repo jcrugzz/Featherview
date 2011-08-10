ca0942cd5e3a1d80f48d5909efe5bdd1611d1a94
Henry Modisett <henrymodisett@gmail.com>
2011-08-10 10:19:54 -0400

    fixed some stuff

--- staticFront/j/application.js
+++ staticFront/j/application.js
@@ -17,20 +17,30 @@ $(document).ready(function(){
 
 });
 
-options = false;
-
 setTimeout(function(){
 $('#options').animate({
                 height: '25'
             }, 300,'easeInBack');
+$('#options img').delay(400).fadeIn(300);
 },2000);
+
+options = false;
+
 $('#options').click(function(){
     if(!options) {
+        $('.masonry').animate({marginTop:'60'},150,'easeInCubic');
+        $(this).animate({paddingTop:'25'}, 150,'easeInCubic');
+        $('#optionsBar').animate({height:'50'},150,'easeInCubic');
+        options = true;
+    }
+    else {
         $(this).animate({
-          height:'50'
+          paddingTop:'0'
         }, 150,function(){
-            $('#optionsBar').animate({height:'50'},150)
+            $('#optionsBar').animate({height:'0'},150);
+            $('.masonry').animate({marginTop:'0'},250);
         });
+        options = false;        
     }
 });
 
